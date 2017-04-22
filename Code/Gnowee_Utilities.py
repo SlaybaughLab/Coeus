6adb1ef1495154f0a910865cfbe5785f858e72c0
James Bevins <james.e.bevins@gmail.com>
2017-04-22 15:03:06 -0700

    Removed objective function import and input line

--- Code/Gnowee_Utilities.py
+++ Code/Gnowee_Utilities.py
@@ -26,7 +26,6 @@ import numpy as np
 from SamplingMethods import Initial_Samples
 from MCNP_Utilities import MCNP_Surface, MCNP_Cell, Read_Tally_Output, Read_MCNP_Output, Print_MCNP_Input
 from Utilities import Switch, to_NormDiff, RelativeLeastSquares, FuncThreadWithReturn, Event
-from ObjectiveFunction import ObjectiveFunction
 from math import tan, radians, log
 from random import random
 
