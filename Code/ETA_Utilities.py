d670c86ea4316ebb81f37965a35809c8dc243e9a
James Bevins <james.e.bevins@gmail.com>
2017-04-23 17:26:30 -0700

    Removed objective spectrum information from ETA_Utilities

--- Code/ETA_Utilities.py
+++ Code/ETA_Utilities.py
@@ -4,16 +4,13 @@
 #
 # Contains : Functions and methods required to set ETA contraints and objective functions.  
 #            This module will:
-#            1) Read objective function from input spectrum file
-#            2) Read ETA contraints from ETA constraints file
-#            3) Store ETA related design parameters in ETA class
-#            4) Evaluate fitness of objective function using U-optimility
+#            1) Read ETA contraints from ETA constraints file
+#            2) Store ETA related design parameters in ETA class
+#            3) Evaluate fitness of objective function using U-optimility
 #
 # Author : James Bevins
 #
-# Last Modified: 17Aug16
-#
-# Module TODOs: - Specify defualt spectrum for ETA-Parameters
+# Last Modified: 23April17
 #
 #######################################################################################################
 
@@ -29,8 +26,7 @@ from math import pi
 class ETA_Parameters:
      
     ## Creates an ETA object that stores the ETA design parameters, constraints, and objective function
-    def __init__(self, spectrum_type='normalized differential', spectrum=np.array([]),\
-                 min_fiss=5E8, max_weight=125.0,src=5E15,\
+    def __init__(self, min_fiss=5E8, max_weight=125.0,src=5E15,\
                  tcc_dist=15.24, debris_shield_thickness= 0.3, wall_thickness=0.5, snout_dist=52.14, cover_thickness=1.0, mount_thickness=2.4, 
                  face_radius=5.48, eta_or=9.39, cone_angle=70.22,\
                  debris_shield_mat="Al", struct_mat="Al", fill_mat="Air (dry near sea level)",fissile_mat='Pb',\
@@ -41,14 +37,6 @@ class ETA_Parameters:
                  max_vert=3, max_horiz=7):
         assert len(nas_foil_th)==len(nas_foil_mat), 'The number of thicknesses and materials for the NAS must be equal.'
         assert len(toad_foil_th)==len(toad_foil_mat), 'The number of thicknesses and materials for the TOAD must be equal.'
-        
-        ## str Indicator of objective spectrum type.  This is use to ensure that the calculate spectrum is converted to the 
-        # proper form for comparison and output. Options are: "normalized", "normalized differential", or "normalized lethargy"
-        # [default='normalized differential']
-        self.spectrum_type=spectrum_type
-        ## array Stores the upper energy bin bounds and flux/fluence for each bin
-        # [default=[]]
-        self.spectrum=spectrum
         
         ## float The maximum weight for the ETA assembly entered in kg
         # [default=125.0 [kg]]
@@ -164,8 +152,7 @@ class ETA_Parameters:
         self.min_fiss=min_fiss/(self.src*self.r_toad**2*pi*self.t_toad[ind])
     
     def __repr__(self):
-        return "ETA_Params({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14}, {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, {23}, {24}, {25}, {26}, {27}, {28}, {29}, {30}, {31}, {32}, {33})".format(\
-                self.spectrum_type, self.spectrum,\
+        return "ETA_Params({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14}, {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, {23}, {24}, {25}, {26}, {27}, {28}, {29}, {30}, {31})".format(\
                 self.min_fiss, self.max_weight, self.src,\
                 self.tcc_dist, self.t_ds, self.t_w, self.snout_dist, self.t_c, self.t_m, self.r_f, self.r_o, self.theta,\
                 self.ds_mat, self.struct_mat, self.fill_mat,self.fissile_mat,\
@@ -217,43 +204,7 @@ class ETA_Parameters:
         header += ["Max vertical components = {}".format(self.max_vert)]
         header += ["Max horizontal components = {}".format(self.max_horiz)]
         
-        header += ["Objective function type = {}".format(self.spectrum_type)]
-        header += ["\nObjective function spectra:"]
-        header += ["Energy    Flux"]
-        header ="\n".join(header)+"\n"
-        s = header + "\n".join(["{0:<7}{1}".format(ebin, flux) for ebin, flux in self.spectrum])
-        return s
-
-    ## Parses an objective spectrum csv file. 
-    # The first row contains the spectrum type: "normalized", "normalized differential", or "normalized lethargy"
-    # The first column contains the upper energy bin boundaries. 
-    # The second column contains the flux/fluence of the bin.  
-    def read_obj(self, filename):
-
-        # Open file
-        try: 
-            self.f = open(filename, 'r') 
-            
-            # Determine the type of spectrum
-            self.spectrum_type=self.f.readline().strip()
-            
-            # Store the spectrum
-            spectrum=[]
-            for line in self.f:
-                split_list=line.split(',')
-                spectrum.append([float(split_list[0].strip()),float(split_list[1].strip())])
-                
-            # Close the file
-            self.f.close()
-        except IOError as e:
-            module_logger.error("I/O error({0}): {1}".format(e.errno, e.strerror))
-            module_logger.error("File not found was: {0}".format(filename)) 
-       
-        # Test that the file closed
-        assert self.f.closed==True, "File did not close properly."
-
-        # Save the spectrum as an attribute
-        self.spectrum=np.asarray(spectrum)
+        return "\n".join(header)+"\n"
 
     ## Parses an ETA constraints csv file. 
     #     The key word options are:
