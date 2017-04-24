8be5752fdbe30beba3d2c935ad7da219aeb6aa1d
jamesbevins <james.e.bevins@gmail.com>
2017-04-24 00:04:42 -0700

    And fixed a typo from when I removed the debug statements.

--- Code/Gnowee_Utilities.py
+++ Code/Gnowee_Utilities.py
@@ -540,12 +540,12 @@ def Calc_Fitness(ids, pop, obj, min_fiss=0, max_w=1000):
                 tmp_fit+= 0.1*(min_fiss/fissions[0]-1)
             elif fissions[0] > min_fiss:
                 tmp_fit-= 0.01*(fissions[0]/min_fiss-1)
-            module_logger.debug"fissions[0] = {} and min_fiss = {} ".format(fissions[0],min_fiss))
-            module_logger.debug"Parent ID # {} has fitness = {} from RLS+fissions".format(i,tmp_fit))
+            module_logger.debug("fissions[0] = {} and min_fiss = {} ".format(fissions[0],min_fiss))
+            module_logger.debug("Parent ID # {} has fitness = {} from RLS+fissions".format(i,tmp_fit))
 
             if weight > max_w:
                 tmp_fit+=1E15
-            module_logger.debug"Parent ID # {} has fitness = {} from RLS+fissions+weight".format(i,tmp_fit))
+            module_logger.debug("Parent ID # {} has fitness = {} from RLS+fissions+weight".format(i,tmp_fit))
         
         except:
             module_logger.warning("WARNING: Parent ID # {} MCNP run failed.".format(i,tmp_fit))
